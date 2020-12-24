90c3c3cb874d8d882851653cb11c0d47673537e1
Саня <sanya@Sergejs-MacBook-Air.local>
2020-12-24 16:26:58 +0600

    В класс Singleton добавлен приватный конструктор и метод CreateOrGetLST, возвращающий лист по типу int

--- Models/Singleton.cs
+++ Models/Singleton.cs
@@ -7,8 +7,23 @@ namespace SingletonHomework.Models
     {
         //= new Random().Next(-1000, 1000)
         public static Random rand = new Random();
-        public List<int> LST = new List<int>(100000);
+        public List<int> LST = new List<int>(10000);
 
-        
+        private Singleton()
+        {
+            
+        }
+
+        public List<int> CreateOrGetLST()
+        {
+            if (LST == null)
+            {
+                for (int i = 0; i < LST.Count; i++)
+                {
+                    LST.Add(rand.Next(-1000, 1000));
+                }
+            }
+            return LST;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SingletonHomework.Models;
 
 namespace SingletonHomework
 {
